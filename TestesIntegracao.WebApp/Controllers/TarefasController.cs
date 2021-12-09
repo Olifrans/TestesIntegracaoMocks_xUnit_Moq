844a9bfbf936122a42942cd7d388505fff0fb710
Francisco Oliveira <fransoliveira@gmail.com>
2021-12-09 11:29:01 -0300

    Conclusão Curso TestesIntegracaoMocks_xUnit_Moq

--- TestesIntegracao.WebApp/Controllers/TarefasController.cs
+++ TestesIntegracao.WebApp/Controllers/TarefasController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Alura.CoisasAFazer.WebApp.Models;
-using Alura.CoisasAFazer.Core.Commands;
-using Alura.CoisasAFazer.Services.Handlers;
 using TestesIntegracao.WebApp.Models;
 using TestesIntegracao.Core.Commands;
 using TestesIntegracao.Services.Handlers;
+using TestesIntegracao.Infrastructure;
+using Microsoft.Extensions.Logging;
 
 namespace TestesIntegracao.WebApp.Controllers
 {
@@ -12,22 +11,31 @@ namespace TestesIntegracao.WebApp.Controllers
     [ApiController]
     public class TarefasController : ControllerBase
     {
+        IRepositorioTarefas _repo;
+        ILogger<CadastraTarefaHandler> _logger;
+
+        public TarefasController(IRepositorioTarefas repositorio, ILogger<CadastraTarefaHandler> logger)
+        {
+            this._repo = repositorio;
+            this._logger = logger;
+        }
+
+
         [HttpPost]
         public IActionResult EndpointCadastraTarefa(CadastraTarefaVM model)
         {
-
-
             var cmdObtemCateg = new ObtemCategoriaPorId(model.IdCategoria);
-            var categoria = new ObtemCategoriaPorIdHandler().Execute(cmdObtemCateg);
+            var categoria = new ObtemCategoriaPorIdHandler(_repo).Execute(cmdObtemCateg);
             if (categoria == null)
             {
                 return NotFound("Categoria não encontrada");
             }
 
             var comando = new CadastraTarefa(model.Titulo, categoria, model.Prazo);
-            var handler = new CadastraTarefaHandler();
-            handler.Execute(comando);
-            return Ok();
+            var handler = new CadastraTarefaHandler(_repo, _logger);
+            var resultado = handler.Execute(comando);
+            if (resultado.IsSuccess) return Ok();
+            return StatusCode(500);
         }
     }
 }
